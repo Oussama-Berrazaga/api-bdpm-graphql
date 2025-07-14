0030bce9a3c58acb437f62a3ed3d426418c6ccbc
Oussama Berrazaga <oussema.berrazaga@gmail.com>
2025-07-14 19:03:10 +0100

    Update bdpm_client.js

--- src/bdpm_client.js
+++ src/bdpm_client.js
@@ -7,10 +7,10 @@ const https = require('https');
 function downloadFile(filename, {
     protocol = "https:",
     host = process.env.BDPM_URL_HOST || "base-donnees-publique.medicaments.gouv.fr",
-    path = process.env.BDPM_URL_PATH || "/telechargement.php"
+    path = process.env.BDPM_URL_PATH || "/download/file/"
 } = {}) {
     return new Promise((resolve, reject) => {
-        const url = new URL(`${protocol}//${host}${path}?fichier=${filename}.txt`);
+        const url = new URL(`${protocol}//${host}${path}${filename}.txt`);
         const timer = `Downloaded ${filename}`;
         console.time(timer);
         const req = https.request(url, res => {
